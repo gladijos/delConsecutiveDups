913f4a599ae9170356d4d3a6484e9983d524ec42
Егор Волков <gladijos@gmail.com>
2022-04-17 11:34:04 +0300

    add some comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-#include <cstring>
-#include <iomanip>
-#include <iostream>
-#include <string>
+#include <cstring>  // for memcpy
+#include <iomanip>  // for getline
+#include <iostream> // for cin
 
+static const char eol{'\0'};
+/**
+ * @brief removeDups removes consecutive duplicate characters
+ *
+ * @param pStr income and outgoing characters sequence
+ */
 void removeDups(char *pStr) {
   auto writeIdx = 0;
   auto readIdx = writeIdx;
-  for (; pStr[readIdx] != '\0'; ++readIdx) {
+  for (; pStr[readIdx] != eol; ++readIdx) {
     if (pStr[readIdx] != pStr[writeIdx]) {
       pStr[++writeIdx] = pStr[readIdx];
     }
   }
   if (writeIdx != readIdx) {
-    pStr[writeIdx + 1] = '\0';
+    pStr[writeIdx + 1] = eol;
   }
 }
 
@@ -20,15 +25,19 @@ int main(int /*argc*/, char * /*argv*/ []) {
   std::string inputLine;
 
   while (true) {
-    std::cout << "Please, input string line" << std::endl;
+    printf("Please, input string line\n");
     std::getline(std::cin, inputLine);
+    auto strLen = inputLine.length();
+    auto *data = new char(strLen + 1);
+    memcpy(data, inputLine.c_str(), strLen);
+    data[strLen] = eol;
     // пример использования
-    auto *data = new char(inputLine.length());
-    memcpy(data, inputLine.c_str(), inputLine.length());
+    // наверное после getline из std::cin логичнее использовать std::cout, но в
+    // примере пользования был printf, так и оставил :)
+    printf("INCOME \"%s\"\n", data);
     removeDups(data);
-
-    std::cout << std::quoted(data) << std::endl;
+    printf("RESULT \"%s\"\n", data);
     delete data;
   }
-  return 1;
+  return 0;
 }
